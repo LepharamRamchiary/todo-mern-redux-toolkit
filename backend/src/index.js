25d7b8d00e7fd62fb65c2efcffb1c854765f7b6a
LepharamRamchiary <lepharamchiary@gmail.com>
2025-08-02 14:10:56 +0530

    conncet with MongoDB done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+import dotenv from "dotenv/config";
+import connectDB from "./db/db.js";
+import { app } from "./app.js";
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo DB connection failed!", err);
+  });
