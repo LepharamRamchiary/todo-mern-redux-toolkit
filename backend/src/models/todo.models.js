ac9abef65f8165e1bb98af88d378c1a9debd0a8d
LepharamRamchiary <lepharamchiary@gmail.com>
2025-08-02 14:20:26 +0530

    add todo model

---
+++ backend/src/models/todo.models.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const todoSchema = new Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Todo = mongoose.model("Todo", todoSchema);
